fef817c3111618731e0c2bb4b556c6888070c22f
pisethtieb <pisethsomaly@hotmail.com>
2015-10-18 21:46:25 +0700

    fixed result data and go rout

---
+++ laboratory/server/collections_cache/result.js
@@ -0,0 +1,4 @@
+////// Collection
+////Labo.Collection.Customer.cacheTimestamp();
+Laboratory.Collection.Patient.cacheCount('laboCount', Laboratory.Collection.Labo, 'patientId');
+Laboratory.Collection.Patient.cacheDoc('agent', Laboratory.Collection.Agent, ['name']);
